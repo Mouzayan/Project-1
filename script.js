3133c6f4baba8399068c39e0335bc1a9bc468912
MaK <42501101+Mouzayan@users.noreply.github.com>
2020-03-10 19:28:28 -0400

    progress

--- script.js
+++ script.js
@@ -16,16 +16,15 @@ form.addEventListener('submit', async (event) => {
     let keyWord = document.querySelector("#keyword").value;
 
 
-    let api_url = `https://newsapi.org/v2/everything?q=${keyWord}&pageSize=40&apiKey=a4001be03ea64ef6981019bb086ecba0`; 
+    let api_url = `https://newsapi.org/v2/everything?q=${keyWord}&pageSize=100&apiKey=a4001be03ea64ef6981019bb086ecba0`; 
 
     const resp = await axios.get(api_url);
 
     console.log(resp.data.articles);
     
     let articles = resp.data.articles;
-
-    articles.map(article => {
-
+    let middle = document.querySelector('.middle')
+    middle.innerHTML = ''
     
     function shuffle(articles) {
         let currentIndex = articles.length;
@@ -40,43 +39,45 @@ form.addEventListener('submit', async (event) => {
         }
         return shuffle;
     }
-
     shuffle(articles);
 
+    // articles.map(article => {
 
+        for (let i = 0; i < 5; i +=1){
+         
+        if (articles[i].urlToImage !== null){
 
-        if (article.urlToImage !== null){
+            let newDiv = document.createElement("div");
+            let newDek = document.createElement("p");
+            let newTitle = document.createElement("h4");
+            let newInfo3 = document.createElement("h6");
+            let newInfo2 = document.createElement("h6");
+            let newInfo1 = document.createElement("h6");
+            
+            newDiv.classList.add("story");
+            newDiv.innerHTML += `<img class="article-image" src=${articles[i].urlToImage}>`;
+            middle.append(newDiv);
 
-        let newDiv = document.createElement("div");
-        newDiv.classList.add("story");
-        newDiv.innerHTML += `<img class="article-image" src=${article.urlToImage}>`;
-        middle.append(newDiv);
-
-        let newInfo1 = document.createElement("h6");
-        newInfo1.innerHTML += `<h6 class="article-dtls">Article source: ${article.source.name}</h6>`;
+        newInfo1.innerHTML += `<h6 class="article-dtls">Article source: ${articles[i].source.name}</h6>`;
         middle.append(newInfo1);
 
-        let newInfo2 = document.createElement("h6");
-        newInfo2.innerHTML += `<h6 class="article-dtls">Written by: ${article.author}</h6>`;
+        newInfo2.innerHTML += `<h6 class="article-dtls">Written by: ${articles[i].author}</h6>`;
         middle.append(newInfo2);
 
-        let newInfo3 = document.createElement("h6");
         newInfo3.innerHTML += `<h6 class="article-dtls">Date
-        published: ${article.publishedAt}</h6>`;
+        published: ${articles[i].publishedAt}</h6>`;
         middle.append(newInfo3);
 
-        let newTitle = document.createElement("h4");
         newTitle.classList.add("article-title");
-        newTitle.innerHTML += `<h4 class="article-title">${article.title}</h4>`;
+        newTitle.innerHTML += `<h4 class="article-title">${articles[i].title}</h4>`;
         middle.append(newTitle);
 
-        let newDek = document.createElement("p");
         newDek.classList.add("dek");
-        newTitle.innerHTML += `<p class="dek">${article.description}</p>`;
+        newTitle.innerHTML += `<p class="dek">${articles[i].description}</p>`;
         middle.append(newDek);
          } 
  
-})
+}
 
 })
 
