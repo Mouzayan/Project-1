0b388f67b25de0377a7bf332a725470f28837cc3
MaK <42501101+Mouzayan@users.noreply.github.com>
2020-03-13 15:37:51 -0400

    progress

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ const smashedNews = async () => {
     let api_url = "http://newsapi.org/v2/top-headlines?category=technology&pageSize=100&apiKey=a4001be03ea64ef6981019bb086ecba0";
   
     const resp = await axios.get(api_url);
-    console.log("l worekd")
     console.log(resp.data.articles);
     
     let articles = resp.data.articles;
